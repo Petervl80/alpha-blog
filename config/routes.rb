03db0645bdb240a262734e3f0af82aa15a6ca9e8
pedro.vinicius <pedro.vinicius@educ.rec.br>
2025-07-14 15:11:19 -0300

    Add create function

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
 
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
 
   get "up" => "rails/health#show", as: :rails_health_check
 
