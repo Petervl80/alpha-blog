fd16f4acddd757f4e2227732a95249e02ac4809d
pedro.vinicius <pedro.vinicius@educ.rec.br>
2025-07-14 14:20:36 -0300

    Add article listing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
 
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
 
   get "up" => "rails/health#show", as: :rails_health_check
 
