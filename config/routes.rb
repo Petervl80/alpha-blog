1a0ed6a37a1f98b2c23e3f280b98acc45a6524ff
pedro.vinicius <pedro.vinicius@educ.rec.br>
2025-07-23 10:36:29 -0300

    Add update route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
 
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 
   get "up" => "rails/health#show", as: :rails_health_check
 
