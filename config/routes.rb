8840ed16bd70e1a2bdfbd622cffde8bdefa0a759
pedro.vinicius <pedro.vinicius@educ.rec.br>
2025-07-23 11:30:19 -0300

    Add delete route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
 
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
 
   get "up" => "rails/health#show", as: :rails_health_check
 
